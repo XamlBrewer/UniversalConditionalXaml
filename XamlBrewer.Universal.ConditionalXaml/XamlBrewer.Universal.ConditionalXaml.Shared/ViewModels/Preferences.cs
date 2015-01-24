1d0ddca1cb00e6165361b02a705b74d06c84e9e7
XamlBrewer <diederik_krols@hotmail.com>
2015-01-24 09:27:51 +0100

    Preferences viewmodel.

---
+++ XamlBrewer.Universal.ConditionalXaml/XamlBrewer.Universal.ConditionalXaml.Shared/ViewModels/Preferences.cs
@@ -0,0 +1,15 @@
+﻿using Mvvm;
+
+namespace XamlBrewer.Universal.ConditionalXaml.ViewModels
+{
+    public class Preferences : BindableBase
+    {
+        private bool isMetric = true;
+
+        public bool IsMetric
+        {
+            get { return this.isMetric; }
+            set { this.SetProperty(ref this.isMetric, value); }
+        }
+    }
+}
